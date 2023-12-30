6629d458d4cc824c1bfe79d9a6c50938cdeb4a91
Minh Hieu <hieutranminh61@gmail.com>
2023-12-30 19:37:27 +0700

    Update Validate

--- SignUp.js
+++ SignUp.js
@@ -87,10 +87,23 @@ function Validator(options) {
                     var formValues = Array.from(enableInputs).reduce((values, input) => {
                         
                         switch(input.type) {
-                            case 'checkbox': 
                             case 'radio': 
                                 values[input.name] = formElement.querySelector('input[name="'+ input.name +'"]:checked').value;
                                 break;
+                            case 'checkbox':
+                                if(!input.matches(':checked')) {
+                                    values[input.name] = '';
+                                    return values;
+                                };
+
+                                if (!Array.isArray(values[input.name])){
+                                    values[input.name] = [];
+                                }
+                                values[input.name].push(input.value);
+                                break;
+                            case 'file': 
+                                values[input.name] = input.files;
+                                break; 
                             default: 
                                 values[input.name] = input.value;
                         }                        
